a7ceeef4981667fdaffddc81a6906f46470037c1
Jie Kang <jkang@redhat.com>
2017-01-31 10:49:52 -0500

    Fix ipa-client-setup to also configure sssd for ifp

--- docker/ipa-docker/ipa-client-setup.sh
+++ docker/ipa-docker/ipa-client-setup.sh
@@ -5,3 +5,6 @@ HOSTNAME=$1
 ipa-getkeytab -s $( awk '/^server/ { print $3 }' /etc/ipa/default.conf ) -k /etc/http.keytab -p HTTP/${HOSTNAME}
 chown apache /etc/http.keytab
 chmod 600 /etc/http.keytab
+
+sed -i 's|[ifp]|[ifp]\nallowed_uids = apache, root|g' /etc/sssd/sssd.conf
+sed -i 's|services = nss, sudo, pam|services = nss, sudo, pam, ifp|g' /etc/sssd/sssd.conf
